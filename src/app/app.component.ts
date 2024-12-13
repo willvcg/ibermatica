d27cecb3f88a9b10fbd247c4fac210db78e7fee3
Will Cumbicus <wilmanvc93@gmail.com>
2024-12-13 19:31:39 +0100

    refactor: update router outlet in appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApplicantsEmployeesService } from './services/applicants-employees.service';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
