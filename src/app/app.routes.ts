6ee7edc578188a798780f7dfad2cd2a7bc8a6c71
Will Cumbicus <wilmanvc93@gmail.com>
2024-12-13 19:32:15 +0100

    feat: create routes with components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('./pages/user-list/user-list.component').then(m => m.UserListComponent) },
+    { path: 'user-detail/:userId', loadComponent: () => import('./pages/user-detail/user-detail.component').then(m => m.UserDetailComponent) },
+    { path: 'user-create', loadComponent: () => import('./pages/user-form/user-form.component').then(m => m.UserFormComponent) },
+    { path: 'user-edit/:userId', loadComponent: () => import('./pages/user-form/user-form.component').then(m => m.UserFormComponent) },
+];
